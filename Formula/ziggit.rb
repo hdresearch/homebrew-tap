ea0b794861a969feb9997a940c05b2cd9d56bbbf
Yev Barkalov <yevbar1729@gmail.com>
2026-04-07 10:33:10 -0700

    ziggit 0.1.7

--- Formula/ziggit.rb
+++ Formula/ziggit.rb
@@ -1,30 +1,30 @@
 class Ziggit < Formula
   desc "A modern git implementation written in pure Zig — 4-10× faster, 142KB WASM, succinct mode for LLMs"
   homepage "https://github.com/hdresearch/ziggit"
-  version "0.1.5"
+  version "0.1.7"
   license "GPL-2.0-only"
 
   on_macos do
     on_arm do
       url "https://github.com/hdresearch/ziggit/releases/download/v#{version}/ziggit-macos-aarch64"
-      sha256 "6dd817b9e946775532184fb83388ae4960565482600bf939fae5273576c94416"
+      sha256 "5c8389568072bd960ab25713e0a24d9ae6817907543086a4103f0717c348e6d5"
     end
 
     on_intel do
       url "https://github.com/hdresearch/ziggit/releases/download/v#{version}/ziggit-macos-x86_64"
-      sha256 "293ef0c4e728f71d63b94e0214c4832b911777c0db1afdbde6dc071a8e72bbea"
+      sha256 "6761265c52423a8d6c9884b75c8abb03758d0e82fff099b5990e10fd5a62f04c"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/hdresearch/ziggit/releases/download/v#{version}/ziggit-linux-aarch64"
-      sha256 "deb078e4417275b4ed459b4e91473f06fa0ecd7eca9f4718dabd3e6388627ce3"
+      sha256 "ceee08d473acf51d120104828be90347db1cc7532d6962a38b162f27d04a82ae"
     end
 
     on_intel do
       url "https://github.com/hdresearch/ziggit/releases/download/v#{version}/ziggit-linux-x86_64"
-      sha256 "7d5076ac68c51121a5771841eafeab2eb8b5439545c7eb2f25b8662a08e9e29c"
+      sha256 "82caee46a2cabf23f5a7f22e2efa60d2dcac1981a8992d0e1bd7114036e57b08"
     end
   end
 
